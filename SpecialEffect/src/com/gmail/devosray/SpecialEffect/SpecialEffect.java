28b54b59f4fe32c2cb85dbf0d860a934a5cec8a6
Raymond de Vos <devosray@gmail.com>
2013-09-29 12:00:40 +0200

    initial commit, set up project

---
+++ SpecialEffect/src/com/gmail/devosray/SpecialEffect/SpecialEffect.java
@@ -0,0 +1,13 @@
+
+package com.gmail.devosray.SpecialEffect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Raymond
+ */
+
+public class SpecialEffect extends JavaPlugin{
+    
+}
